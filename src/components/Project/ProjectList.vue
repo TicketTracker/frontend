3c1c4763edd953105e26cef5fda4633426d41e11
Viktor <v.melnychuk12@gmail.com>
2018-04-18 21:43:13 +0300

    list projects

--- src/components/Project/ProjectList.vue
+++ src/components/Project/ProjectList.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="hello">
-      <ul>
-          <li v-for="p in projects" :key="p._id">{{p._id}}</li>
-      </ul>
+  <div class="projects">
+    <div class="container">
+    <h3>All projects:</h3>
+      <div class="list-group">
+        <a v-for="p in projects" :key="p._id" href="#" class="list-group-item list-group-item-action">
+          <div>
+            <h4>{{p.name}}</h4>
+            <p>{{p.description}}</p>
+          </div>
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,12 +25,19 @@ export default {
   },
   beforeMount() {
     const projSer = new ProjectService();
-    this.projects = projSer.getAllProjects();
+    projSer
+      .getAllProjects()
+      .then(data => (this.projects = data.data))
+      .catch(err => {
+        throw err;
+      });
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.projects {
+  margin-top: 1.5em;
+}
 </style>
